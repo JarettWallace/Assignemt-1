d5896be2cf368abc4ba8b3e8e40649ecd33c88a1
JarettWallace <Jwallace02@rio.edu>
2025-09-07 23:31:45 -0400

    product_catalog.py

--- product_catalog.py
+++ product_catalog.py
@@ -1,10 +1,12 @@
 from product_data import products
 # TODO: Step 1 - Print out the products to see the data that you are working with.
-
+print("Available Products:")
+for product in products:
+    print(product)
 
 
 # TODO: Step 2 - Create a list called customer_preferences and store the user preference in this list.
-
+customer_preferences = [] 
 
 response = ""
 while response != "N":
@@ -12,15 +14,21 @@ while response != "N":
     preference = input()
     # Add the customer preference to the list
 
+    customer_preferences.append(preference)
     response = input("Do you want to add another preference? (Y/N): ").upper()
   
 
 # TODO: Step 3 - Convert customer_preferences list to set to eliminate duplicates.
-
+customer_tags = set(customer_preferences)
 
 
 # TODO: Step 4 - Convert the product tags to sets in order to allow for faster comparisons.
 converted_products = []
+for product in products:
+    converted_products.append({
+        "name": product["name"],
+        "tags": set(product["tags"])
+        ])
 
 
 
@@ -36,7 +44,7 @@ def count_matches(product_tags, customer_tags):
     '''
     pass
 
-
+return len(product_tags.intersection(customer_tags))
 
 
 # TODO: Step 6 - Write a function that loops over all products and returns a sorted list of matches
@@ -49,14 +57,35 @@ def recommend_products(products, customer_tags):
         list: A list of products containing product names and their match counts.
     '''
     pass
+    
+    recommendations = [] 
+    for product in products:
+        matches = count_matches(profuct["tags"], customer_tags)
+        if matches > 0:
+            reccomendations.append({
+                "name": product["name"],
+                "matches": matches
+            }) 
+
+    recommendations.sort(key = lambda x: (-x["matches"], x["name"]))
+    return recommendations
 
 
 
-# TODO: Step 7 - Call your function and print the results
+
+# TODO: Step 7 - Call your function and print the results 
+recommended = recommend_products(converted_products, customer_tags)
+
+print("\nRecommmended Products:")
+for item in recommended:
+    print(f"- {item['name']} ({item['matches']} match(es))")
 
 
 
 
-# DESIGN MEMO (write below in a comment):
-# 1. What core operations did you use (e.g., intersections, loops)? Why?
-# 2. How might this code change if you had 1000+ products?
+
+# DESIGN MEMO (write below in a comment): should be around 209 word -Jarett
+# 1 Throughout my code I used several different core functions that are present within python; these functions include things like for loops and sorting. To start, loops are used mainly to help check every single product against what the customers preferences are, if this loop was not present we couldn't compare each product to each other leading to my code being very inefficient. Next up I used sorting functions through the use of lambda, the use of this function helps make sure my code is working efficiently as possible along with making sure the product with the most matches to the customers specifications will appear first. Both of these functions are used to ensure my code is as efficient as possible and meets the requirements of the code. 
+# 2 If the amount of products grows over one thousand items it would be a good idea to prestore any tags as sets, it would also be important for the code to limit the top results as to not overwhelm any potential customers as showing every item that can relate to their result could show several items that they would not want to see. Limiting the results will also lead to more stream line results and letting a customer make different searches quickly. 
+
+
